e7451daf6870a985e4d939c42e64024924aa063b
peterffydominika <peterffy.dominika@gmail.com>
2026-03-23 12:56:51 +0100

    Add project files.

---
+++ NASACLI/Program.cs
@@ -0,0 +1,83 @@
+﻿
+namespace NASACLI
+{
+    public class Program
+    {
+        public static List<Kuldetes> kuldetesek = new List<Kuldetes>();
+        static void Main(string[] args)
+        {
+            Beolvas();
+            Feladat3();
+            Feladat4();
+            Feladat5();
+            Feladat6();
+            //MaxTeher();
+        }
+
+        private static void MaxTeher() {
+            double max = kuldetesek[0].HasznosTeher;
+            int index = 0;
+            foreach (var item in kuldetesek)
+            {
+                if (item.HasznosTeher > max)
+                {
+                    max = item.HasznosTeher;
+                    index = kuldetesek.IndexOf(item);
+                }
+            }
+            Console.WriteLine($"\n6.Feladat: A legnagyobb hasznos teher: {kuldetesek[index].HasznosTeher} kg ({kuldetesek[index].Nev})");
+        }
+
+        private static void Feladat6() {
+            double min = kuldetesek[0].HasznosTeher;
+            int index = 0;
+            foreach (var item in kuldetesek)
+            {
+                if (item.HasznosTeher < min)
+                {
+                    min = item.HasznosTeher;
+                    index = kuldetesek.IndexOf(item);
+                }
+            }
+            Console.WriteLine($"\n6.Feladat: A legkisebb hasznos teher: {kuldetesek[index].HasznosTeher} kg ({kuldetesek[index].Nev})");
+        }
+
+        private static void Feladat5() {
+            Console.WriteLine("5.Feladat: Küldetések kockázati szintjei:");
+            foreach (var kuldetes in kuldetesek)
+            {
+                if (kuldetes.KockazatiSzint() == "Magas")
+                {
+                    Console.WriteLine($"{kuldetes.Nev} - Kockázati szint: {kuldetes.KockazatiSzint()}");
+                }
+            }
+        }
+
+        private static void Feladat4() {
+            string keresettReszlet;
+            Kuldetes keresett = null;
+            do
+            {
+                Console.Write($"4.Feladat: Adja meg egy küldetés nevének részletét: ");
+                keresettReszlet = Console.ReadLine();
+                keresett = kuldetesek.LastOrDefault(k => k.Nev.ToUpper().Contains(keresettReszlet.ToUpper()));
+            }
+            while (keresett == null);
+            Console.WriteLine($"Talált küldetés: {keresett.Nev}, {keresett.Ev}, {keresett.Celpont}, {(keresett.Sikeres == true?"Sikeres":"Sikertelen")}\n");
+
+            
+        }
+
+        private static void Feladat3() {
+            Console.WriteLine($"3.Feladat: {kuldetesek.Count} küldetés található az állományban.\n");
+        }
+
+        public static void Beolvas() {
+            StreamReader sr = new StreamReader("NASAmissions.txt");
+            sr.ReadLine();
+            while (!sr.EndOfStream) {
+                kuldetesek.Add(new Kuldetes(sr.ReadLine()));
+            }
+        }
+    }
+}
---
+++ NASACLITests/KuldetesTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NASACLI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NASACLI.Tests {
+    [TestClass()]
+    public class KuldetesTests {
+
+        [TestMethod()]
+        [DataRow(24.0, 4600.0, "Közepes")]
+        [DataRow(0.0, 4600.0, "Alacsony")]
+        [DataRow(24.0, 15000.0, "Magas")]
+        [DataRow(24.0, 25000.0, "Magas")]
+        /*
+         * "Magas" ha a költség meghaladja az 1 milliárd USD-t és a rakomány tömege több mint 10 tonna.
+            "Közepes" ha csak az egyik feltétel teljesül.
+            "Alacsony" ha egyik feltétel sem teljesül.
+         */
+        public void KockazatiSzintTest(double koltseg, double tomeg, string expected) {
+            Kuldetes kuldetes = new Kuldetes($"Test;2024;Mars;5;Igen;Test leiras;{koltseg};{tomeg}");
+            string actual = kuldetes.KockazatiSzint();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
