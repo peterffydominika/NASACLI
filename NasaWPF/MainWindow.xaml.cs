4fcc6032ed7c881066d3e04fa52fd26515f26152
peterffydominika <90351361+peterffydominika@users.noreply.github.com>
2026-03-30 08:51:41 +0200

    Add statistics button functionality to MainWindow

--- NasaWPF/MainWindow.xaml.cs
+++ NasaWPF/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -27,6 +27,18 @@ namespace NasaWPF {
         private void dtgDatas_SelectionChanged(object sender, SelectionChangedEventArgs e) {
             lblTitle.Content = (dtgDatas.SelectedItem as Kuldetes).Nev;
             progressBar.Value = (dtgDatas.SelectedItem as Kuldetes).HasznosTeher;
+        }
+
+        private void btnStatistic_Click(object sender, RoutedEventArgs e)
+        {
+            lblTitle.Content = "Statisztika";
+            progressBar.Value = 0;
+            List<Statisztika> statisztika = new List<Statisztika>();
+            List<Kuldetes> Emberes = Program.kuldetesek.Where(x => x.Legenyseg > 0).ToList();
+            List<Kuldetes> NemEmberes = Program.kuldetesek.Where(x => x.Legenyseg == 0).ToList();
+            statisztika.Add(new Statisztika("Emberes küldetések", Emberes.Count, $"{Emberes.Average(x => x.HasznosTeher):F2} kg", $"{Emberes.Average(x => x.Koltseg):F2} mrd USD$"));
+            statisztika.Add(new Statisztika("Nem emberes küldetések", NemEmberes.Count, $"{NemEmberes.Average(x => x.HasznosTeher):F2} kg", $"{NemEmberes.Average(x => x.Koltseg):F2} mrd USD$"));
+            dtgDatas.ItemsSource = statisztika;
         }
     }
 }
